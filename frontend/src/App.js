41b42e799245c1c9437740eaeace63971bc55e6d
Harry Dennen <h@harrydennen.com>
2024-11-12 13:36:18 -0500

    change chart to match mock up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,18 @@ const data = [
     after: 600,
     amt: 550,
   },
+  {
+    name: 'Area 4',
+    before: 800,
+    after: 650,
+    amt: 725,
+  },
+  {
+    name: 'Area 5',
+    before: 700,
+    after: 600,
+    amt: 650,
+  },
 
 ];
 
@@ -28,15 +40,15 @@ const areaDot = {stroke: '#ffe5a9', strokeWidth: 2, fill: 'white', r: 5}
 function App() {
   return (
     <div className="App">
-        <ComposedChart width={730} height={250} data={data} barGap={20}>
+        <ComposedChart width={730} height={450} data={data} barGap={20}>
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <CartesianGrid stroke="#f5f5f5" />
-          <Area type="monotone" dataKey="amt" dot={areaDot} fill="#ffe5a9" stroke="#ffe5a9" />
-          <Bar dataKey="before" barSize={20} fill="#8db1d3"/>
-          <Bar dataKey="after" barSize={20} fill="#50cb71" left={10} right={10}/>
+          <CartesianGrid stroke="#dadbdd" />
+          <Area type="monotone" dataKey="amt" name="Average" dot={areaDot} fill="#ffe5a9" stroke="#ffe5a9" />
+          <Bar dataKey="before" name="Confidence (Before)" barSize={20} fill="#8db1d3"/>
+          <Bar dataKey="after" name="Confidence (After)" barSize={20} fill="#50cb71"/>
         </ComposedChart>
     </div>
   );
